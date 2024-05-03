f3283278bcb1090de89d0aa5275bf4d9b71247fc
Prakhar <rinayush@gmail.com>
2024-05-03 16:11:32 +0530

    added

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleNav() {
+    var navItems = document.querySelector('.navbar ul');
+    navItems.classList.toggle('show'); // Toggle the 'show' class to display/hide nav items
+  }
+  
