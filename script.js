e002857d628788b36c406660b5cd64dd5c985909
Prakhar <rinayush@gmail.com>
2024-05-03 18:20:50 +0530

    changes done

--- script.js
+++
@@ -1,5 +0,0 @@
-function toggleNav() {
-    var navItems = document.querySelector('.navbar ul');
-    navItems.classList.toggle('show'); // Toggle the 'show' class to display/hide nav items
-  }
-  
